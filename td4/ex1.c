64861671b10f0b9cc2c7230402c44af961d42817
redouanesoul <soulredouane@gmail.com>
2022-10-03 09:23:18 +0200

    ex1 is donne

--- td4/ex1.c
+++ td4/ex1.c
@@ -1,38 +1,11 @@
 #include<stdio.h>
-#define pi=3.14
-void main(){
-    float rayon, p;
-    int n,i=1;
-    printf("entrer le nombre de circle que vous souhaiter calculer leurs périmètres:");
+void main (){
+    int i =0,n;
+    printf("donner le nombre des elements que vous souhaiter afficher:");
     scanf("%d",&n);
-    do
+    for ( i = 0; i <=n; i++)
     {
-        printf("donner le rayon de la circle de nombre %d:",i);
-        scanf("%f",&rayon);
-        p=2*pi*rayon; //there is a probleme here
-        printf("le périmètres de votre circle est:%f",p);
-        i++;
-
-    } while (i<n); 
+      printf("%d \n",i);
+    }
     
 }
-
-
-
-// void main(){
-//      float rayon, p;
-//      printf("donner le rayon de la circle ");
-//     while (rayon>=0)
-//     {
-//     scanf("%f",&rayon);
-//     p=2*pi*rayon; //there is a probleme here
-//     printf("le périmètres de votre circle est:%f",p);
-//     printf("donner le rayon de la circle de nombre :");
-//     }
-    
-// }
-
-
-
-
-
