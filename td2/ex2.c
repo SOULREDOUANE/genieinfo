a8b8f7ba5e624e758148ad482944e4668b7ed4e8
redouanesoul <redwanesol2000@gmail.com>
2022-10-16 20:09:19 +0100

    fixing

--- td2/ex2.c
+++ td2/ex2.c
@@ -2,7 +2,7 @@
 void main(){
     float a ,b ;
     printf("entrer ici les deux constants caracteristique(a puis b) de votre equationde  1ere ordre:");
-    scanf("%f %f ",&a,&b);
+    scanf("%f%f ",&a,&b);
 if (a!=0){
         printf("l'unique solution the l'equation est:%f",-b/a); }
     else if (a=0 && b!=0)
