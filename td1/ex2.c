fc45c4b86920e4b6d8dadc64b9e8086bb47da74e
redouanesoul <redwanesol2000@gmail.com>
2022-10-16 16:16:52 +0100

    fixing some problems

--- td1/ex2.c
+++ td1/ex2.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include<math.h>
 int main(){
-    const float p=3.14;
+    const float p=M_PI;
     float rayon, perimetre ;
     printf("donner le rayon de votre circle:");
-    scanf("%f",rayon);
+    scanf("%f",&rayon);
     perimetre= 2* p * rayon;
-    printf("le perimtre de votre circle est:%f",perimetre);
+    printf("le perimetre de votre circle est:%f",perimetre);
     return 0;
 }
